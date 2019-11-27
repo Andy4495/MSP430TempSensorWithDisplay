fe43053428e08a052dfa0a4eda4bc6ec7fab72cb
Andreas Taylor <Andy4495@outlook.com>
2019-11-27 11:02:44 -0600

    Updates to reduce power usage, other cleanup.

--- README.md
+++ README.md
@@ -18,6 +18,26 @@ Because of some BoosterPack/LaunchPad pin incompatibilities, it is necessary to 
 
 To decrease overall power consumption, disconnect the emulation section of the LaunchPad -- remove all jumpers from J101.
 
+## Library Modifications ##
+
+This uses an updated and modified version of the `LCD_SharpBoosterPack_SPI` library, rather than version 1.0.0 included with Energia.
+
+Copy version 1.0.3 from [GitHub][10] and make the following modifications to `LCD_SharpBoosterPack_SPI.cpp`:
++ To make the constructor definitions equivalent, update the second constructor definition (lines 106 - 108) and replace this:
+```    
+digitalWrite(RED_LED, HIGH);
+lcd_vertical_max = model;
+lcd_horizontal_max = model;
+```
+with this:
+```
+lcd_vertical_max = model;
+lcd_horizontal_max = model;
+static uint8_t * _frameBuffer;
+_frameBuffer = new uint8_t[_index(lcd_vertical_max, lcd_horizontal_max)];
+DisplayBuffer = (uint8_t *) _frameBuffer;
+```
+
 ## Program details ##
 
 The sketch collects the following data:
@@ -35,6 +55,8 @@ The sketch collects the following data:
 After collecting the sensor data, the data is packaged and transmitted to a
 receiver hub which can then further process and store the data over time.
 
+There is a conflict with the library's use of the OneMsTaskTimer and sleep(), such that sleep() does not work properly. Although the LCD [datasheet][11] discusses the procedure to refresh the display at a minimum of once per second, it turns out that the refresh is not necessary. Therefore, the sketch disables the automatic screen refresh function (by setting the autoVCOM parameter in the constructor to false). This allows the microcontroller to sleep most of the time, drastically reducing power requirements. Per EnergyTrace measurements, the module pulls a mean current draw of 0.027 mA.
+
 ## LaunchPad and BoosterPack Pin usage
 ```
 Pin    FR2433  Fuel Tank CC110L SHARP96  Comment
@@ -44,7 +66,7 @@ Pin    FR2433  Fuel Tank CC110L SHARP96  Comment
  3      RXD
  4      TXD
  5                              LCD_EN
- 6
+ 6                              LCD_CS
  7      SCK                     SCK
  8
  9      SCL       SCL                    Software I2C. Fuel Tank hardware modified to move I2C to pins 9/10 instead of 14/15
@@ -75,6 +97,7 @@ Pin    FR2433  Fuel Tank CC110L SHARP96  Comment
 * [430BOOST-SHARP96][4] Display BoosterPack
     * Note that this BoosterPack has been discontinued. This sketch is specific to this BoosterPack and may require modifications to work with the [SHARP128][9] BoosterPack
 * [Wireless Sensor Receiver Hub][5]
+* Version 1.0.3 of [LCD_SharpBoosterPack_SPI library][10]
 
 
 [1]: http://www.ti.com/tool/MSP-EXP430FR2433
@@ -86,3 +109,5 @@ Pin    FR2433  Fuel Tank CC110L SHARP96  Comment
 [7]: https://github.com/Andy4495/SWI2C
 [8]: http://www.ti.com/tool/BOOSTXL-BATPAKMKII
 [9]: http://www.ti.com/tool/BOOSTXL-SHARP128
+[10]: https://github.com/energia/Energia/tree/master/libraries/LCD_SharpBoosterPack_SPI
+[11]: https://www.mouser.com/datasheet/2/365/LS013B4DN04(3V_FPC)-1202885.pdf
--- Workshop_Temp_Sensor_FR2433.ino
+++ Workshop_Temp_Sensor_FR2433.ino
@@ -1,4 +1,3 @@
-
 /* -----------------------------------------------------------------
     MSP430 Remote Temp Sensor with LCD Display
     https://github.com/Andy4495/MSP430TempSensorWithDisplay
@@ -16,6 +15,12 @@
                       -- per EnergyTrace, does not reduce power
                       -- seems to get stuck in a ~16 second delay, regardless of value
                       -- There may be a conflict with the 1 ms timer used to refresh the SHARP96
+    11/26/19 - A.T. - Use updated SHARP96 library (version 1.7 from Energia GitHub, slightly modified)
+                      Call .end() method before sleeping. This seems to work around the conflict with the
+                      ms timer used for refresh from interfering with sleep timing.
+                      - Drastically cuts down on power usage without impacting display readability.
+                      Added a couple #ifdefs so that sketch works with or without FuelTank
+                      Cleaned up some comments
 
 */
 /* -----------------------------------------------------------------
@@ -47,7 +52,7 @@
      3      RXD
      4      TXD
      5                              LCD_EN
-     6
+     6                              LCD_CS
      7      SCK                     SCK
      8
      9      SCL       SCL                   Software I2C. Fuel Tank hardware modified to move I2C to pins 9/10 instead of 14/15
@@ -65,12 +70,15 @@
 */
 
 // If using the Fuel Tank BoosterPack (Version 1, not Version 2),
-// then uncomment the following 3 lines which will then send
+// then uncomment the following line which will then send
 // voltage levels from the Fuel Tank and not directly from Vcc.
 // Fuel tank shuts down at ~3.65 LiPo voltage
 #define FUEL_TANK_ENABLED
+
+#ifdef FUEL_TANK_ENABLED
 #define SWI2C_ENABLED
 #include "SWI2C.h"
+#endif
 
 // Comment out following line if testing without CC110L BoosterPack
 #define RADIO_ENABLED
@@ -81,20 +89,25 @@
 
 #include "OneMsTaskTimer.h"
 #include "LCD_SharpBoosterPack_SPI.h"
-LCD_SharpBoosterPack_SPI myScreen;
+static const uint8_t SHARP_CS   = 6;
+static const uint8_t SHARP_DISP = 5;
+static const uint8_t SHARP_VCC  = 2;
+//LCD_SharpBoosterPack_SPI(uint8_t pinChipSelect, uint8_t pinDISP, uint8_t pinVCC, bool autoVCOM, uint8_t model)
+LCD_SharpBoosterPack_SPI myScreen(SHARP_CS, SHARP_DISP, SHARP_VCC, false, SHARP_96);
+
 int orientation = 1;
 
 // Fuel Tank Register Addresses
-#define BQ27510_Control                     0x00
-#define BQ27510_Temperature                 0x06
-#define BQ27510_Voltage                     0x08
-#define BQ27510_Flags                       0x0a
-#define BQ27510_NominalAvailableCapacity    0x0c
-#define BQ27510_FullAvailableCapacity       0x0e
-#define BQ27510_RemainingCapacity           0x10
-#define BQ27510_FullChargeCapacity          0x12
-#define BQ27510_AverageCurrent              0x14
-#define BQ27510_TimeToEmpty                 0x16
+#define BQ27510_Control                        0x00
+#define BQ27510_Temperature                    0x06
+#define BQ27510_Voltage                        0x08
+#define BQ27510_Flags                          0x0a
+#define BQ27510_NominalAvailableCapacity       0x0c
+#define BQ27510_FullAvailableCapacity          0x0e
+#define BQ27510_RemainingCapacity              0x10
+#define BQ27510_FullChargeCapacity             0x12
+#define BQ27510_AverageCurrent                 0x14
+#define BQ27510_TimeToEmpty                    0x16
 #define BQ27510_G3_StandbyCurrent              0x18
 #define BQ27510_G3_StandbyTimeToEmpty          0x1a
 #define BQ27510_G3_StateOfHealth               0x1c
@@ -132,6 +145,9 @@ struct sPacket
 struct sPacket txPacket;
 
 MspTemp myTemp;
+#ifndef FUEL_TANK_ENABLED
+MspVcc myVcc;
+#endif
 
 #ifdef SWI2C_ENABLED
 #define SDA_PIN 10
@@ -149,7 +165,7 @@ uint8_t   data8;
 char text[MAXTEXTSIZE];      // buffer to store text to print
 
 unsigned int loopCount = 0;
-const unsigned long sleepTime = 61;  // In seconds
+const unsigned long sleepTime = 240;  // In seconds. FR2433 seems to be off by factor of 4 (so 240 => 60 seconds)
 
 void setup() {
 
@@ -203,7 +219,7 @@ void loop() {
   myScreen.text(1, 10, text);
   snprintf(text, MAXTEXTSIZE, "Vcc (mV): %d", txPacket.sensordata.Batt_mV);
   myScreen.text(1, 19, text);
-  snprintf(text, MAXTEXTSIZE, "Min Rmn:  %d", txPacket.sensordata.Millis);
+  snprintf(text, MAXTEXTSIZE, "Min Rmn:  %lu", txPacket.sensordata.Millis);
   myScreen.text(1, 28, text);
   myScreen.setFont(1);
   snprintf(text, MAXTEXTSIZE, "Temp");
@@ -215,7 +231,6 @@ void loop() {
   myScreen.text(64, 85, text);
   myScreen.flush();
 
-  delay(sleepTime * 1000UL); // sleep() and sleepSeconds() do not appear to work correctly on FR2433
-  ///  sleepSeconds(sleepTime);
+  sleepSeconds(sleepTime);
 
 }
